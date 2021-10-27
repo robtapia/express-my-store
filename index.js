b7c9e953aa63343a2ebea86d0ea49f929eed1dde
Roberto Tapia <roberto.tapia@ug.uchile.cl>
2021-10-27 16:39:29 -0300

    endpoints with parameters

--- index.js
+++ index.js
@@ -4,8 +4,40 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!')
-})
+});
+
+app.get('/products', (req,res)=>{
+    res.json([
+        {
+            name: 'Product 1',
+            price: 1000
+        },
+        {
+            name: 'Product 2',
+            price: 2000
+        }
+    ])
+});
+
+app.get('/products/:id', (req, res)=>{
+    // const id = req.params.id;
+    const { id } = req.params;
+    res.json({
+        id,
+        name: 'Product 2',
+        price: 2000
+    });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res)=>{
+    const {categoryId, productId} = req.params;
+    res.json({
+        categoryId,
+        productId
+    })
+});
+
 
 app.listen(port, () => {
     console.log('Port:'+port)
-})
+});
